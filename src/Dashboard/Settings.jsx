442ae67546c09f9f6230b011c3d4461d6985b6c0
Elias Moreno <elias.cmoreno17@gmail.com>
2019-09-30 14:43:38 -0500

    Added boilerplate to files

---
+++ src/Dashboard/Settings.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Settings
+			</div>
+        )
+    }
+
+}
