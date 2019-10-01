5fc8202e7c444f2f85869c52be575b09b6cf62df
Elias Moreno <elias.cmoreno17@gmail.com>
2019-10-01 16:25:41 -0500

    initial attempt and success for displaying map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Main from './Main';
 import './App.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
-	return (
-		<div className="App">
-			<Main/>
-		</div>
-	);
+
+export class App extends React.Component {
+	render() {
+		return (
+			<Map 
+				google={this.props.google} 
+				zoom={14}
+				initialCenter={{
+					lat: 33.5779,
+					lng: -101.8552
+				}}
+			>
+				<Marker onClick={this.onMarkerClick}
+					name={'Current location'} />
+
+			</Map>
+		);
+	}
 }
 
-export default App;
+export default GoogleApiWrapper({
+	apiKey: 'AIzaSyBbtQVWV6W90rXN8wIHpj0FjF1uqD0ov00'
+})(App)
